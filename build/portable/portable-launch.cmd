f0b372d461db578b63a020852ced146de464d800
ruxi-hc <44031703+ruxi-hc@users.noreply.github.com>
2026-07-24 15:02:42 +0800

    build: update portable startup cache version

--- build/portable/portable-launch.cmd
+++ build/portable/portable-launch.cmd
@@ -2,7 +2,7 @@
 setlocal EnableExtensions DisableDelayedExpansion
 
 set "ARCHIVE=%~dp0BlackHole-RecycleBin.zip"
-set "APP_VERSION=1.0.0"
+set "APP_VERSION=1.0.2"
 set "APP_ROOT=%LOCALAPPDATA%\BlackHoleRecycleBin\%APP_VERSION%"
 set "APP_EXE=%APP_ROOT%\Black Hole Recycle Bin.exe"
 set "READY_FILE=%APP_ROOT%\.ready"
